7457d20598f121b0f567b4cc297dc96c798e38d8
karyaerginerr <karyaerginerr@gmail.com>
2025-12-22 17:46:16 +0100

    Initial commit

---
+++ karya_lab10/src/test/java/com/example/karya_lab10/KaryaLab10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.karya_lab10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaryaLab10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
